c7c2aa542502d68503c8c11de8bfdba02d2eefa2
Carles Mateu <carlesm@carlesm.com>
2017-09-23 01:27:23 +0200

    Fixes

--- exemple-6-4.py
+++ exemple-6-4.py
@@ -1,20 +1,11 @@
-# These two lines are because of the folder the demos are located in, and aren't normally necessary
-import os.path, sys
-sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
-
-# Minecraft Turtle Example
-# Ported from the scratch turtle project in "Adventures in Raspberry Pi"
 from minecraftstuff import MinecraftTurtle
 from mcpi import minecraft
 from mcpi import block
 
-# Connect to minecraft server 127.0.0.1 as player 'steve'
 mc = minecraft.Minecraft.create(address="193.144.12.6", name="test1")
 
-# get players position
 pos = mc.player.getPos()
 
-# create minecraft turtle
 turtle = MinecraftTurtle(mc, pos)
 turtle.speed(0)
 turtle.setheading(90)
